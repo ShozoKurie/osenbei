cc43f2f6991c69e5ecdd1ed15af53f4c5b6c1a40
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-21 18:17:06 +0900

    製品の新規登録を定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,55 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  # before_action :set_item, only: [:show, :edit, :update, :destroy]
+  # before_action :shut_other_users, only: [:edit, :update, :destroy]
+
   def index
+    # 表示順をレビューもしくはツイートが更新された順に変更する。
+    @items = Item.all.order(created_at: :desc)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :maker, :region_id, :url, :item_image)
+  end
+
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
+
 end
+
+
+
+
+
+def show
+end
+
+def edit
+end
+
+private
+
+
+
+# def shut_other_users
+#   if current_user != @item.user
+#     redirect_to root_path
+#   end
+# end
+
